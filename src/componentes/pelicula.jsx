77e20f25a1c0238ee1cc966d78a16ced2e91b16c
SebastianMujica22 <42355577+SebastianMujica22@users.noreply.github.com>
2020-05-22 09:30:49 -0500

    table

--- src/componentes/pelicula.jsx
+++ src/componentes/pelicula.jsx
@@ -4,10 +4,10 @@ export default function Pelicula(props) {
   let pelicula = props.pelicula;
   return (
     <tr>
-      <th scope="row">props.indice</th>
-      <td>props.pelicula.name</td>
-      <td>props.pelicula.directedBy</td>
-      <td>props.pelicula.country</td>
+      <th scope="row">{props.indice}</th>
+      <td>{props.pelicula.name}</td>
+      <td>{props.pelicula.directedBy}</td>
+      <td>{props.pelicula.country}</td>
     </tr>
   );
 }
